68ef31a37f78ed7e7be412b1d3c6d699a866ba40
HJulian Mejia <stereohj-dev@gmail.com>
2024-05-26 14:00:23 -0500

    VARIANT method to KEK key applied

--- 3DES-Variant_test.py
+++ 3DES-Variant_test.py
@@ -13,7 +13,41 @@
 # *     with the corresponding working key (WK) variant (byte).
 # * 3)  Use the resulting key to encrypt the WK.
 
-from os import urandom                                                      # Cryptographic secure PRNG (From Operating System)
+
 from cryptography.hazmat.primitives.ciphers.algorithms import TripleDES     # Algorithm class
 from cryptography.hazmat.primitives.ciphers import Cipher, modes            # Mode of operation class
-from cryptography.hazmat.primitives import padding                          # Padding class
+
+
+# Key Encryption Key (KEK): 3DES-Double key length (16 bytes / 32 hex / 128 bits)
+KEK = '2ABC3DEF4567018998107645FED3CBA2'    # Hex notation
+b_KEK = bytearray.fromhex(KEK)              # Bytes *mutable* object
+
+# Key PIN Encryption (KPE): 3DES-Single key length (8 bytes / 16 hex / 64 bits)
+KPE = '0123456789ABCDEF'                    # Hex notation
+b_KPE = bytearray.fromhex(KPE)              # Bytes *mutable* object
+
+# KPE VARIANT
+VARIANT_1 = '08'                            # Hex notation
+b_VARIANT_1 = bytearray.fromhex(VARIANT_1)  # Bytes *mutable* object
+
+
+# ** Left KEK "Variant" part
+
+# * STEP 1: Get the index of the most significant byte (MS-byte)
+lb_kek_index = 0                   
+
+# * STEP 2:  Get the MS-byte and apply XOR with Variant byte
+lb_var_kek = int.to_bytes( b_KEK[lb_kek_index] ^ int.from_bytes(b_VARIANT_1) )
+
+
+# ** Right KEK "Variant" part
+
+# * STEP 1: Get the index of the MS-byte
+rb_kek_index = (len(b_KEK) // 2)
+
+# * STEP 2:  Get the MS-byt and apply XOR with Variant byte
+rb_var_kek = int.to_bytes( b_KEK[rb_kek_index] ^ int.from_bytes(b_VARIANT_1) )
+
+
+# ** STEP 2: Get the Complete KEK "Variant 1"
+b_KEK_VAR_1 = lb_var_kek + b_KEK[1 : rb_kek_index] + rb_var_kek + b_KEK[rb_kek_index + 1 :] 
