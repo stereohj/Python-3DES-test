daa671053594ce4c30c0bcafe309f3e910054754
HJulian Mejia <stereohj-dev@gmail.com>
2024-05-28 23:36:59 -0500

    3DES VARIANT example finished

--- 3DES-Variant_test.py
+++ 3DES-Variant_test.py
@@ -26,9 +26,9 @@ b_KEK = bytearray.fromhex(KEK)              # Bytes *mutable* object
 KPE = '0123456789ABCDEF'                    # Hex notation
 b_KPE = bytearray.fromhex(KPE)              # Bytes *mutable* object
 
-# KPE VARIANT
-VARIANT_1 = '08'                            # Hex notation
-b_VARIANT_1 = bytearray.fromhex(VARIANT_1)  # Bytes *mutable* object
+# KPE VARIANT 
+VARIANT = '08'                          # KPE has the 08 Hex byte assigned (VARIANT 1)  
+b_VARIANT = bytearray.fromhex(VARIANT)  # Bytes *mutable* object
 
 
 # ** Left KEK "Variant" part
@@ -37,7 +37,7 @@ b_VARIANT_1 = bytearray.fromhex(VARIANT_1)  # Bytes *mutable* object
 lb_kek_index = 0                   
 
 # * STEP 2:  Get the MS-byte and apply XOR with Variant byte
-lb_var_kek = int.to_bytes( b_KEK[lb_kek_index] ^ int.from_bytes(b_VARIANT_1) )
+lb_var_kek = int.to_bytes( b_KEK[lb_kek_index] ^ int.from_bytes(b_VARIANT) )
 
 
 # ** Right KEK "Variant" part
@@ -46,8 +46,38 @@ lb_var_kek = int.to_bytes( b_KEK[lb_kek_index] ^ int.from_bytes(b_VARIANT_1) )
 rb_kek_index = (len(b_KEK) // 2)
 
 # * STEP 2:  Get the MS-byt and apply XOR with Variant byte
-rb_var_kek = int.to_bytes( b_KEK[rb_kek_index] ^ int.from_bytes(b_VARIANT_1) )
+rb_var_kek = int.to_bytes( b_KEK[rb_kek_index] ^ int.from_bytes(b_VARIANT) )
 
 
-# ** STEP 2: Get the Complete KEK "Variant 1"
-b_KEK_VAR_1 = lb_var_kek + b_KEK[1 : rb_kek_index] + rb_var_kek + b_KEK[rb_kek_index + 1 :] 
+# ** STEP 2: Get the Complete KEK "Variant"
+b_KEK_VAR = lb_var_kek + b_KEK[1 : rb_kek_index] + rb_var_kek + b_KEK[rb_kek_index + 1 :]
+KEK_VAR  = b_KEK_VAR.hex().upper()
+
+hf_kek_index = len(KEK) // 2
+
+print("\n*** 3DES VARIANT EXAMPLE ***\n")
+
+print(f"VARIANT 1 (KPE - Hex notation): {VARIANT}\n")
+
+print("KEK (Hex notation):\t\t", KEK[0 : 2], KEK[2 : hf_kek_index], 
+      KEK[hf_kek_index : hf_kek_index + 2], KEK[hf_kek_index + 2: ], sep=' ')
+
+print("KEK VARIANT (Hex notation):\t", KEK_VAR[0 : 2], KEK_VAR[2 : hf_kek_index], 
+      KEK_VAR[hf_kek_index : hf_kek_index + 2], KEK_VAR[hf_kek_index + 2: ], sep=' ')
+
+print(f"\nKPE (Hex notation): {KPE}")
+
+
+# ** STEP 3: Encrypt the KPE under KEK VARIANT
+
+# * Specify algorithm and operation mode
+algorithm = TripleDES(b_KEK_VAR)
+cipher = Cipher(algorithm, modes.ECB())
+
+
+# * Define the Cipher Context in "encryption direction/form"
+encryptor = cipher.encryptor()
+c_kpe = encryptor.update(b_KPE)     # "Charge" KPE on to "encryption" Cipher Context
+c_kpe += encryptor.finalize()       # When all KPE is treated, the Cipher Context must be finalized
+
+print(f"ENCRYPTED KPE by KEK VARIANT (Hex notation): {c_kpe.hex().upper()}\n")
