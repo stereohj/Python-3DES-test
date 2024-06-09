5556410949273ef0005d639dfe1bf1ec125ba7f3
HJulian Mejia <stereohj.dev@gmail.com>
2024-06-08 22:05:46 -0500

    Key Check digits generation added on variant example

--- 3DES-Variant_test.py
+++ 3DES-Variant_test.py
@@ -7,28 +7,50 @@
 # ! TDEA / Triple-DES / 3DES ALGORITHM is obsolete and ***SHOULD NOT BE USED***
 # ! VARIANT method is obsolete and ***SHOULD NOT BE USED***
 
-# * To produce variants:
+from cryptography.hazmat.primitives.ciphers.algorithms import TripleDES     # Algorithm class
+from cryptography.hazmat.primitives.ciphers import Cipher, modes            # Mode of operation class
+
+
+# *** Key Check Digits function (3DES)
+# * Encrypt a zeros data block with the key and take leftmost 3 bytes from the result
+def key_check_digits(key: bytes) -> bytes:
+
+      # ! ALERT: Algorithms classes can take keys an apply it some 
+      # ! changes (e.g. most cryptographic systems needs 3DES keys
+      # ! to be odd parity. So every rightmost bit -RSB- in a key
+      # ! byte can be changed to "converit" in an odd parity byte).
+      # ! For this reason USE the bytes *unmutable* object.
+      # ! (DO NOT USE a bytearray *mutable* object).
+      algorithm = TripleDES(key)      
+      cipher = Cipher(algorithm, modes.ECB())
+
+      zeros_data = bytes(len(key))  # key length must be 8 bytes / 16 hex / 64 bits multiple
+      
+      encryptor = cipher.encryptor()      
+      kcv = encryptor.update(zeros_data)  # "Charge" "zeros_data" on to "encryption" Cipher Context  
+      kcv += encryptor.finalize()         # When all data is treated, the Cipher Context must be finalized
+      
+      return kcv[0:3]
+
+
+# *** To produce VARIANTS cryptograms:
 # * 1)  Split the KEK/ZMK (Key Encryption Key / Zone Master Key) in two halves.
 # * 2)  Take the most significant byte of each part and apply XOR 
 # *     with the corresponding working key (WK) variant (byte).
 # * 3)  Use the resulting key to encrypt the WK.
 
 
-from cryptography.hazmat.primitives.ciphers.algorithms import TripleDES     # Algorithm class
-from cryptography.hazmat.primitives.ciphers import Cipher, modes            # Mode of operation class
-
-
 # Key Encryption Key (KEK): 3DES-Double key length (16 bytes / 32 hex / 128 bits)
-KEK = '2ABC3DEF4567018998107645FED3CBA2'    # Hex notation
-b_KEK = bytearray.fromhex(KEK)              # Bytes *mutable* object
+KEK = '2ABC3DEF4567018998107645FED3CBA2'  # Hex notation
+b_KEK = bytes.fromhex(KEK)                # Bytes object
 
-# Key PIN Encryption (KPE): 3DES-Single key length (8 bytes / 16 hex / 64 bits)
-KPE = '0123456789ABCDEF'                    # Hex notation
-b_KPE = bytearray.fromhex(KPE)              # Bytes *mutable* object
+# Key PIN Encryption (KPE): 3DES-Single key length (16 bytes / 32 hex / 128 bits)
+KPE = '0123456789ABCDEFFEDCBA9876543210'  # Hex notation
+b_KPE = bytes.fromhex(KPE)                # Bytes object
 
 # KPE VARIANT 
-VARIANT = '08'                          # KPE has the 08 Hex byte assigned (VARIANT 1)  
-b_VARIANT = bytearray.fromhex(VARIANT)  # Bytes *mutable* object
+VARIANT = '08'                            # KPE has the 08 Hex byte assigned (VARIANT 1)  
+b_VARIANT = bytes.fromhex(VARIANT)        # Bytes object
 
 
 # ** Left KEK "Variant" part
@@ -61,11 +83,13 @@ print(f"VARIANT 1 (KPE - Hex notation): {VARIANT}\n")
 
 print("KEK (Hex notation):\t\t", KEK[0 : 2], KEK[2 : hf_kek_index], 
       KEK[hf_kek_index : hf_kek_index + 2], KEK[hf_kek_index + 2: ], sep=' ')
+print(f"KEK KCV (Hex notation): {key_check_digits(b_KEK).hex().upper()}\n")
 
 print("KEK VARIANT (Hex notation):\t", KEK_VAR[0 : 2], KEK_VAR[2 : hf_kek_index], 
       KEK_VAR[hf_kek_index : hf_kek_index + 2], KEK_VAR[hf_kek_index + 2: ], sep=' ')
 
-print(f"\nKPE (Hex notation): {KPE}")
+print(f"\nKPE (Hex notation): {b_KPE.hex().upper()}")
+print(f"KPE KCV (Hex notation): {key_check_digits(b_KPE).hex().upper()}\n")
 
 
 # ** STEP 3: Encrypt the KPE under KEK VARIANT
@@ -73,7 +97,6 @@ print(f"\nKPE (Hex notation): {KPE}")
 # * Specify algorithm and operation mode
 algorithm = TripleDES(b_KEK_VAR)
 cipher = Cipher(algorithm, modes.ECB())
-
 
 # * Define the Cipher Context in "encryption direction/form"
 encryptor = cipher.encryptor()
